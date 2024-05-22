7df7db807ea264a204b4daafc9d54f9dd64aa8f6
Dimaggio <zodiak-d@yandex.ru>
2024-05-22 16:49:40 +0300

    2.9. Добавил `AuthenticationController` и AuthenticationService`.
    Генерировать можно не только модули, но и заготовки для других базовых
    абстракций Nest. Например, чтобы сгенерировать контроллер для модуля `AuthenticationModule` воспользуемся командой:
    
    ```
    nx generate @nx/nest:controller app/authentication --project=account
    ```
    
    После выполнения команды, в директории модуль `authentication` будет создан
    новый файл `authentication.controller.ts` (заготовка для контроллера) и файл
    `authentication.controller.spec.ts` (заготовка для тестов). Файл с тестами можно сразу удалить, пока они не потребуются.
    
    Аналогичным образом происходит подготовка заготовок для сервисов.
    
    NX позволяет описывать произвольные генераторы с учётом особенностей конкретного проекта. Про эту возможность можно почитать в документации.
    
    P.S. Обратите внимание, мы используем библиотеки для описания модулей. То есть не генерируем в `--project`.

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -4,3 +4,7 @@ export { User } from './lib/interfaces/user.interface';
 export { AuthUser } from './lib/interfaces/auth-user.interface';
 
 export { UserRole } from './lib/enums/user-role.enum';
+
+export { StorableEntity } from './lib/interfaces/storable-entity.interface';
+export { EntityFactory } from './lib/interfaces/entity-factory.interface';
+
