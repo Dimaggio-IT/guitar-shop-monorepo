59075e6b7f802bc77944788a88d979643920efa3
Dimaggio <zodiak-d@yandex.ru>
2024-05-22 16:50:09 +0300

    мелкие правки

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -2,9 +2,7 @@ export { Entity } from './lib/base/entity';
 
 export { User } from './lib/interfaces/user.interface';
 export { AuthUser } from './lib/interfaces/auth-user.interface';
-
-export { UserRole } from './lib/enums/user-role.enum';
-
 export { StorableEntity } from './lib/interfaces/storable-entity.interface';
 export { EntityFactory } from './lib/interfaces/entity-factory.interface';
 
+export { UserRole } from './lib/enums/user-role.enum';
