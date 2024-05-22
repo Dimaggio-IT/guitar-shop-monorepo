534f5ede2f8cea93a425a1082a56c6951ecf4e51
Dimaggio <zodiak-d@yandex.ru>
2024-05-23 00:02:31 +0300

    2.20. Добавил метод `comparePassword`.
    Следующим шагом решим задачу проверки пароля пользователя. Когда
    пользователь захочет авторизоваться, он передаст пароль в открытом виде. Полученный пароль нужно сравнить с хешем, который хранится в базе данных.
    
    Для решения этой задачи опишем метод `comparePassword`. Аргументом он принимает пароль в открытом виде. Внутри метода происходит сравнение с помощью функцию `compare` из пакета `bcrypt`. Результатом станет булево значение: `true` — пароль совпадает с хешем, `false` — нет.

--- project/libs/account/shop-user/src/shop-user-module/shop-user.entity.ts
+++ project/libs/account/shop-user/src/shop-user-module/shop-user.entity.ts
@@ -1,4 +1,4 @@
-import { genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 
 import { Entity } from '@project/shared/core';
 import { StorableEntity, AuthUser, UserRole } from '@project/shared/core';
@@ -42,5 +42,9 @@ export class ShopUserEntity extends Entity implements StorableEntity<AuthUser> {
     const salt = await genSalt(SALT_ROUNDS);
     this.passwordHash = await hash(password, salt);
     return this;
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
   }
 }
