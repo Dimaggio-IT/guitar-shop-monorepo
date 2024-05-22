ed04457ee24699c46f3df96a8d54b57cac2c0ee8
Dimaggio <zodiak-d@yandex.ru>
2024-05-22 17:25:16 +0300

    добавил тип для helpers `TPlainObject`

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,8 +1,10 @@
-export { Entity } from './lib/base/entity';
-
 export { User } from './lib/interfaces/user.interface';
 export { AuthUser } from './lib/interfaces/auth-user.interface';
 export { StorableEntity } from './lib/interfaces/storable-entity.interface';
 export { EntityFactory } from './lib/interfaces/entity-factory.interface';
+
+export { TPlainObject } from './lib/types/plain-object.type';
+
+export { Entity } from './lib/base/entity';
 
 export { UserRole } from './lib/enums/user-role.enum';
--- project/libs/shared/helpers/src/lib/common.ts
+++ project/libs/shared/helpers/src/lib/common.ts
@@ -1,26 +1,25 @@
 import { ClassTransformOptions, plainToInstance } from 'class-transformer';
+import { TPlainObject } from '@project/shared/core';
 
-type PlainObject = Record<string, unknown>;
-
-export function fillDto<T, V extends PlainObject>(
+export function fillDto<T, V extends TPlainObject>(
   DtoClass: new () => T,
   plainObject: V,
   options?: ClassTransformOptions,
 ): T;
 
-export function fillDto<T, V extends PlainObject[]>(
+export function fillDto<T, V extends TPlainObject[]>(
   DtoClass: new () => T,
   plainObject: V,
   options?: ClassTransformOptions,
 ): T[];
 
-export function fillDto<T, V extends PlainObject>(
+export function fillDto<T, V extends TPlainObject>(
   DtoClass: new () => T,
   plainObject: V,
   options?: ClassTransformOptions,
 ): T | T[] {
   return plainToInstance(DtoClass, plainObject, {
-      excludeExtraneousValues: true,
-      ...options,
+    excludeExtraneousValues: true,
+    ...options,
   });
 }
