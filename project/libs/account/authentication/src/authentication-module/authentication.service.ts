50639b2cf6d270a3ac9d84108e7f23183cc9b6b4
Dimaggio <zodiak-d@yandex.ru>
2024-05-23 00:12:48 +0300

    2.22. Добавил метод `verifyUser` в `AuthenticationService`.
    Аналогичным образом добавим в сервис `AuthService` методы `verifyUser`
    и `getUser`. В первом выполним проверку логина и пароля
    пользователя. Для этого соберём сущность пользователя и воспользуемся
    методом `comparePassword`.
    
    В методе `getUser` реализуем получение пользователя по уникальному
    идентификатору.

--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,11 +1,11 @@
-import dayjs from 'dayjs';
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 import { ShopUserRepository, ShopUserEntity } from '@project/shop-user';
 import { UserRole } from '@project/shared/core';
 
 import { CreateUserDto } from '../dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './authentication.constant';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constant';
+import { LoginUserDto } from '../dto/login-user.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -30,9 +30,34 @@ export class AuthenticationService {
       throw new ConflictException(AUTH_USER_EXISTS);
     }
 
-    const userEntity = await new shopUserEntity(shopUser).setPassword(password);
+    const userEntity = await new ShopUserEntity(shopUser).setPassword(password);
 
     return this.shopUserRepository
       .save(userEntity);
   }
- }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.shopUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    if (!await existUser.comparePassword(password)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return existUser;
+  }
+
+  public async getUser(id: string) {
+    const user = await this.shopUserRepository.findById(id);
+
+    if (! user) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    return user;
+  }
+}
