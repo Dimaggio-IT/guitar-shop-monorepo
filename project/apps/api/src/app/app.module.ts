e6eca57d1f628e31b3f597513a392d0e01654953
Ratel <zodiak-d@yandex.ru>
2024-06-10 23:34:29 +0300

    удалил консоль-логи

--- project/apps/api/src/app/app.module.ts
+++ project/apps/api/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
 
-// import { ShopUserModule } from '@project/shop-user';
 import { ShopProductModule } from '@project/shop-product';
 import { AuthenticationModule } from '@project/authentication'
 import { ConfigurationModule } from '@project/configuration';
-// import { PrismaClientModule } from '@project/shared/models';
-// import { MailModule } from '@project/mail';
 
 @Module({
   imports: [
     ConfigurationModule,
     AuthenticationModule,
-    // PrismaClientModule,
-    // ShopUserModule,
     ShopProductModule,
-    // MailModule,
   ],
   controllers: [],
   providers: [],
